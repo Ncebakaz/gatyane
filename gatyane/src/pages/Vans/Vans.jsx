a22ef0181307aa460545c1e7d3919b78d0b6d7cd
Ncebakazi Somdaka <109283030+Ncebakaz@users.noreply.github.com>
2024-11-04 10:13:57 +0200

    Update Vans.jsx

--- gatyane/src/pages/Vans/Vans.jsx
+++ gatyane/src/pages/Vans/Vans.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { getVans } from "../api";
 
-
+export function loader() {
+    return getVans()
+}
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
+    const [error, setError] = React.useState(null)
+    const vans = useLoaderData()
 
     const typeFilter = searchParams.get("type")
-    console.log(searchParams.toString())
-
-    React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-
-    }, [])
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
@@ -22,7 +18,11 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-title">
-            <Link to={van.id} state={{ search: searchParams.toString() }}>
+            <Link to={van.id}
+                state={{
+                    search: `searchParams.toString()}`,
+                    type: typeFilter
+                }}>
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -43,17 +43,30 @@ export default function Vans() {
             return prevParams
         })
     }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
     return (
         <div className="van-list-container">
             <h1>Explore our options</h1>
             <div className='van-list-filter-button'>
                 <button
                     onClick={() => handleFilterChange('type', 'simple')}
-                    className="van-type simple"
+                    className={
+                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                    }
                 >Simple</button>
-                <button onClick={() => handleFilterChange('type', 'luxury')} className="van-type luxury">luxury</button>
-                <button onClick={() => handleFilterChange(' type', 'rugged')} className="van-type rugged">rugged</button>
-                <button onClick={() => handleFilterChange('type', null)} className="van-type clear-filters ">Clear filters</button>
+                <button onClick={() => handleFilterChange('type', 'luxury')}
+                    className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                    }
+                >luxury</button>
+                <button onClick={() => handleFilterChange(' type', 'rugged')}
+                    className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}
+                >rugged</button>
+                <button onClick={() => handleFilterChange('type', null)}
+                    className="van-type clear-filters "
+                >Clear filters</button>
             </div>
             <div className="van-list">
                 {vanElements}
